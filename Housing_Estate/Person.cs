6aedc71cc53a121b7ffb853e38213dc3a1b63177
Jaroslav Ondruš <jaroslavondrus05@gmail.com>
2020-11-27 13:47:41 +0100

    Add project files.

---
+++ Housing_Estate/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Housing_Estate
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, string surName, int age)
+        {
+            Name = name;
+            SurName = surName;
+            Age = age;
+
+        }
+
+        public override string ToString()
+        {
+            return ($"MENO: {Name}\nPRIEZVISKO: {SurName}\nVEK: {Age}");
+        }
+    }
+}
---
+++ Housing_Estate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Housing_Estate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Jaro", "Ondruš", 4);
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
